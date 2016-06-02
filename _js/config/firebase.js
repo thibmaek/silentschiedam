c15228021423988fe84064db1160d80b9ddb5356
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-06-02 16:56:55 +0200

    Missing private key for Firebase broke the build, fixes it.

--- _js/config/firebase.js
+++ _js/config/firebase.js
@@ -3,7 +3,8 @@ import Firebase from 'firebase';
 const config = {
   apiKey: 'AIzaSyD81KfPLZ57NcbUTDI6sZdcaJwM7F8H4JI',
   authDomain: 'silentschiedam.firebaseapp.com',
-  databaseURL: 'silentschiedam.firebaseio.com'
+  databaseURL: 'silentschiedam.firebaseio.com',
+  serviceAccount: './_js/config/key.json'
 };
 
 Firebase.initializeApp(config);
