6953009941e74747f595c694fa3182429eb78dff
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-06-02 16:03:41 +0200

    Added & fixed public assets

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: ['url-loader?limit=5000&name=../public/img/img-[hash:6].[ext]']
+        loaders: ['url-loader?limit=5000&name=../img/img-[hash:6].[ext]']
       }
     ]
   },
