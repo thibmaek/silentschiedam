ef6cda991874e68cd0cefaeec2815fabc3b91ea6
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-06-07 21:25:02 +0200

    Added a preloader when fetching content for Detail

--- _js/pages/Detail.jsx
+++ _js/pages/Detail.jsx
@@ -79,6 +79,16 @@ export default class Detail extends Component {
           </section>
         </div>
       );
+    } else {
+      return(
+        <div className='spinner'>
+          <div className='rect1'></div>
+          <div className='rect2'></div>
+          <div className='rect3'></div>
+          <div className='rect4'></div>
+          <div className='rect5'></div>
+        </div>
+      );
     }
   }
 
